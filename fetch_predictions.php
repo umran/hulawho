aa3365efe39efa011c2cfdfc5fe7fd767e2ae2a8
Umran Hussein <umran.hussein@gmail.com>
2016-05-28 18:30:36 +0500

    updates

--- fetch_predictions.php
+++ fetch_predictions.php
@@ -16,11 +16,7 @@ echo "<table >";
 while ($row = mysqli_fetch_assoc($pd_exec)){
 	
 		echo "<tr>
-		<td>".date('m-d-Y H:i:s', $row['time'])."</td>
-		<td><img class='thumb' src='images/icons/".strtolower($row['team_a_flag']).".gif'/>&nbsp".$row['team_a_name']."</td>
-		<td><div class='score'>".$row['score_a']." - ".$row['score_b']."</div></td>
-		<td><div class='pull-right'>".$row['team_b_name']."&nbsp<img class='thumb' src='images/icons/".strtolower($row['team_b_flag']).".gif'/></div></td>
-		<td>"."</td>
+		<td><div class='row'><div class='col-xs-3'><div class=''>".date('m-d-Y', $row['time'])."</div></div><div class='col-xs-3'><div class=''><img class='thumb_left' src='images/icons/".strtolower($row['team_a_flag']).".gif'/>".$row['team_a_name']."</div></div><div class='col-xs-3'><div class='score'>".$row['score_a']." - ".$row['score_b']."</div></div><div class='col-xs-3'><div class='pull-right'>".$row['team_b_name']."<img class='thumb_right' src='images/icons/".strtolower($row['team_b_flag']).".gif'/></div></div></div></td>
 		</tr>";
 	
 }
