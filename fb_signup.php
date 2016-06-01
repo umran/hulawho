3647be9363ff28f6a6924b72ca585274d67f90d8
Umran Hussein <umran.hussein@gmail.com>
2016-06-02 02:37:29 +0500

    major debug

--- fb_signup.php
+++ fb_signup.php
@@ -1,21 +1,28 @@
 <?php
 require('init.php');
 
-if (!empty($_POST["username"]) && !empty($_SESSION["fb_access_token"]) && !empty($_SESSION["fb_id"])) {
+if(!empty($_POST["username"]) && !empty($_SESSION["fb_access_token"]) && !empty($_SESSION["fb_id"])) {
   
   $sanuser = mysqli_real_escape_string($_POST["username"]);
   $sanfb_id = mysqli_real_escape_string($_SESSION["fb_id"]);
 
-  $checkuserquery = "SELECT * FROM users WHERE username = '".$sanuser."'";
+  $checkuserquery = "SELECT * FROM users WHERE username =".$sanuser;
+  
+  $existsQuery = "SELECT * FROM users WHERE fb_id =".$sanfb_id;
+  
   $inputquery = "INSERT INTO users (username, fb_id) VALUES ('".$sanuser."', '".$sanfb_id."')";
   $q = "INSERT INTO rankings (username, gd_total, total) VALUES ('".$sanuser."', 0, 0)";
 
   $queryuser = mysqli_query($con, $checkuserquery);
+  $execExistsQuery = mysqli_query($con, $existsQuery);
 
   $errors = array();    
 
   if (mysqli_num_rows($queryuser) == 1) {
     print "<div class='alert alert-info'><strong><i class='fa fa-info-circle'></i> Oops! The username is not available as it has already been taken.</strong>";
+    exit;
+  } elseif (mysqli_num_rows($execExistsQuery) == 1) {
+    print "<div class='alert alert-info'><strong><i class='fa fa-info-circle'></i> Oops! This facebook account already has a username associated with it.</strong>";
     exit;
   }
 		
@@ -32,6 +39,8 @@ if (!empty($_POST["username"]) && !empty($_SESSION["fb_access_token"]) && !empty
     $_SESSION["username"] = $sanuser;
     $_SESSION["loggedin"] = 1;
     echo "<div class='alert alert-success'><strong><i class='fa fa-check-circle-o'></i> Registration Successful</strong> You may now <a href='index.php'>Sign In</a> to your account.</div>";
+    echo $sanuser;
+    echo $sanfb_id;
   }
 }
 
