cf97234484ae3ec622e504d8655f1a30932287e1
Umran Hussein <umran.hussein@gmail.com>
2016-06-10 07:46:44 +0500

    corrected time comparisons

--- fetch_games.php
+++ fetch_games.php
@@ -9,7 +9,7 @@ $q = "SELECT * FROM matches";
 $q_exec = mysqli_query($con, $q);
 
 while ($row = mysqli_fetch_assoc($q_exec)){
-	$match_time = $row['unix_time'] - 21600;	
+	$match_time = $row['unix_time'];	
 	if($match_time>$time){
 		echo "<option value='".$row['match_id']."'>".$row['team_a']." vs ".$row['team_b']."</option>\n";
 	}
